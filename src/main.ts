1f61f66fab5a5136b5d9e8ac297678b36a8ae223
seef-elshnawy <seefelshnawy@gmail.com>
2024-03-19 10:50:15 +0200

    Refactor main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { existsSync, mkdirSync, writeFile } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const env = process.env
+  if (env.NODE_ENV === 'production') {
+    const dir = 'logs';
+    if (!existsSync(dir)) {
+      mkdirSync(dir, {
+        recursive: true
+      });
+      writeFile('logs/logs.out', '', (err: any) => {
+        if (err) console.log(err);
+      });
+    }
+  }
+  const app = await NestFactory.create(AppModule,{
+    cors: false
+  });
+  const port = process.env.PORT || 3000
+  await app.listen(port);
 }
 bootstrap();
